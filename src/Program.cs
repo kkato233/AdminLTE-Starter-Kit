091d40c87938f07017a6bd2316a3062fcedf229e
Kazuhiro Kato <kazuhiro.kato@hotmail.co.jp>
2024-03-20 20:08:14 +0900

    ASP.NET Core 8.0 対応

--- src/Program.cs
+++ src/Program.cs
@@ -1,26 +1,128 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Company.WebApplication1.Data;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Company.WebApplication1.Services.Mail;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Company.WebApplication1.Services.Profile;
 
-namespace Company.WebApplication1
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.Configure<CookiePolicyOptions>(options =>
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+    // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+    options.CheckConsentNeeded = context => true;
+    options.MinimumSameSitePolicy = SameSiteMode.None;
+});
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
+builder.Services.Configure<CookieTempDataProviderOptions>(options =>
+{
+    options.Cookie.IsEssential = true;
+});
+
+// Add services to the container.
+var Configuration = builder.Configuration;
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+       options.UseSqlServer(connectionString));
+
+
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>(config =>
+{
+    config.User.RequireUniqueEmail = true;    //  email
+    config.User.AllowedUserNameCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 -._@+";
+    config.SignIn.RequireConfirmedEmail = false;
+})
+.AddEntityFrameworkStores<ApplicationDbContext>()
+.AddDefaultTokenProviders();
+
+
+if (Configuration["Authentication:Facebook:IsEnabled"] == "true")
+{
+    builder.Services
+        .AddAuthentication()
+        .AddFacebook(facebookOptions => {
+            facebookOptions.AppId = Configuration["Authentication:Facebook:AppId"];
+            facebookOptions.AppSecret = Configuration["Authentication:Facebook:AppSecret"];
+        });
 }
+
+if (Configuration["Authentication:Google:IsEnabled"] == "true")
+{
+    builder.Services
+        .AddAuthentication()
+        .AddGoogle(googleOptions => {
+            googleOptions.ClientId = Configuration["Authentication:Google:ClientId"];
+            googleOptions.ClientSecret = Configuration["Authentication:Google:ClientSecret"];
+        });
+}
+
+builder.Services.AddRazorPages(options =>
+{
+    options.Conventions.AuthorizeFolder("/");
+
+    options.Conventions.AllowAnonymousToPage("/Error");
+    options.Conventions.AllowAnonymousToPage("/Account/AccessDenied");
+    options.Conventions.AllowAnonymousToPage("/Account/ConfirmEmail");
+    options.Conventions.AllowAnonymousToPage("/Account/ExternalLogin");
+    options.Conventions.AllowAnonymousToPage("/Account/ForgotPassword");
+    options.Conventions.AllowAnonymousToPage("/Account/ForgotPasswordConfirmation");
+    options.Conventions.AllowAnonymousToPage("/Account/Lockout");
+    options.Conventions.AllowAnonymousToPage("/Account/Login");
+    options.Conventions.AllowAnonymousToPage("/Account/LoginWith2fa");
+    options.Conventions.AllowAnonymousToPage("/Account/LoginWithRecoveryCode");
+    options.Conventions.AllowAnonymousToPage("/Account/Register");
+    options.Conventions.AllowAnonymousToPage("/Account/ResetPassword");
+    options.Conventions.AllowAnonymousToPage("/Account/ResetPasswordConfirmation");
+    options.Conventions.AllowAnonymousToPage("/Account/SignedOut");
+});
+
+builder.Services.AddControllersWithViews()
+    .SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
+
+if (Configuration["Email:EmailProvider"] == "SendGrid")
+{
+    builder.Services.Configure<SendGridAuthOptions>(Configuration.GetSection("Email:SendGrid"));
+    builder.Services.AddSingleton<IMailManager, SendGridMailManager>();
+}
+else
+{
+    builder.Services.AddSingleton<IMailManager, EmptyMailManager>();
+}
+
+builder.Services.AddScoped<ProfileManager>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseMigrationsEndPoint();
+    app.UseDeveloperExceptionPage();
+    app.UseDatabaseErrorPage();
+}
+else
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseCookiePolicy();
+app.UseRouting();
+app.UseAuthentication();
+app.UseAuthorization();
+app.MapRazorPages();
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.Run();
